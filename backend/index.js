44b6474d76baea5718e7bbb9b6e13c382354e919
harshilbodara <hasrhilbodara555@gmail.com>
2021-01-01 11:53:07 +0530

    ok

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const chalk = require('chalk');
 const app = express();
 const PORT = process.env.PORT | 5000;
 
-
+console.log("LLLL")
 
 app.listen(PORT,()=>{
     console.log(chalk.black.bgWhiteBright.bold("server runing:",PORT))
